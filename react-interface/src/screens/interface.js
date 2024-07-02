25ed1a3d66d2e516e0120c332b8db14d671954a5
GKenjiMizuno <bibiel.kenji@gmail.com>
2024-07-02 15:10:58 -0300

    Começado a interface

---
+++ react-interface/src/screens/interface.js
@@ -0,0 +1,21 @@
+import React from "react";
+import './interface.css'
+
+const Interface = () => {
+
+
+    return (
+      <div className="body">
+        <div className = 'container'>
+            <div className="container-header">
+                <input id = 'password' name = 'password' type="password" placeholder="Digite a senha"/>
+            </div>
+        </div>
+      </div>  
+
+    )
+
+
+}
+
+export default Interface
