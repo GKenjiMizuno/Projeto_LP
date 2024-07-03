2c435ab73bc3900fcb37d90f1bea8625956685c7
GKenjiMizuno <bibiel.kenji@gmail.com>
2024-07-03 15:15:01 -0300

    Adicionado as rotas

--- react-interface/src/App.js
+++ react-interface/src/App.js
@@ -2,12 +2,18 @@ import './App.css';
 import Login from './screens/login';
 import Home from './screens/home';
 import DataFetcher from './DataFetcher';
+import { BrowserRouter as Router, Route, Switch, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <Home />
-    </div>
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path='/' element={<Login />} />
+          <Route path = '/home' element={<Home />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- react-interface/src/screens/login.js
+++ react-interface/src/screens/login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import './login.css'
+import { Link } from "react-router-dom";
 
 const Login = () => {
 
@@ -9,7 +10,11 @@ const Login = () => {
         <div className = 'container'>
             <div className="container-header">
                 <input id = 'password' name = 'password' type="password" placeholder="Digite a senha"/>
+                <Link to = '/home'>
+                  <button className="button-enviar">Enviar</button>
+                </Link>
             </div>
+          
         </div>
       </div>  
 
